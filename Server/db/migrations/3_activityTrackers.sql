563a5fcc66d35b6ad01482f442373c5ce9e6584f
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-03-30 09:23:38 +0100

    small changes to schema

--- Server/db/migrations/3_activityTrackers.sql
+++ Server/db/migrations/3_activityTrackers.sql
@@ -4,5 +4,6 @@ CREATE TABLE activityTrackers (
     id serial PRIMARY KEY,
     user_ID int NOT NULL,
     habit_ID int NOT NULL,
-    frequency int NOT NULL
+    frequency int NOT NULL,
+    number int NOT NULL
 );
