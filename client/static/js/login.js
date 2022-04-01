470d7add14312d70b6755ffea36fce56375e1f91
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-01 12:32:39 +0100

    fixing url typo

--- client/static/js/login.js
+++ client/static/js/login.js
@@ -1,4 +1,4 @@
-let url = "https://localhost:3000"
+let url = "http://localhost:3000"
 
 async function requestLogin(e) {
     console.log(e)
--- client/static/js/registration.js
+++ client/static/js/registration.js
@@ -1,4 +1,4 @@
-url = "https://localhost:3000"
+url = "http://localhost:3000"
 
 async function register(e) {
   e.preventDefault();
